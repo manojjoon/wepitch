3dfa4e42773a56c2d8dd75293513ce41fc33e518
sanitizekaro <sanitizekaro2020@gmail.com>
2021-02-05 00:49:04 +0530

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
   baseUrl:'http://sachin18-001-site1.btempurl.com/api/',
-  //baseUrl: 'https://localhost:5001/api/',
   baseUrlImage: 'http://sachin18-001-site1.btempurl.com/'
 };
