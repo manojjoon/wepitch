30d6278fcc2e3306cf1bbc0dbf15505675d61418
sanitizekaro <sanitizekaro2020@gmail.com>
2021-02-04 23:39:29 +0530

    imageurl added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,10 @@
 
 export const environment = {
   production: false,
-  //baseUrl: 'http://sachin18-001-site1.btempurl.com/api/',
- baseUrl: 'https://localhost:5001/api/',
- baseUrlImage: 'https://localhost:5001/'
+  baseUrl: 'http://sachin18-001-site1.btempurl.com/api/',
+  baseUrlImage: 'http://sachin18-001-site1.btempurl.com/'
+//  baseUrl: 'https://localhost:5001/api/',
+//  baseUrlImage: 'https://localhost:5001/'
 };
 
 /*
